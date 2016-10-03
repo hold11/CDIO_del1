946d46139c92d591be2304c6059b00e34176ade9
Anders W. Olsen <awo@defalt.net>
2016-10-03 15:22:28 +0200

    Dice docs added

--- src/Dice.java
+++ src/Dice.java
@@ -11,19 +11,34 @@
 
 import java.util.*;
 
+/**
+ * This class is a dice that returns a random value.
+ * @version 1.0.0
+ */
 public class Dice {
     private Random rand;
 
     private static int faceCount;
 
+    /**
+     * When a Dice object gets constructed, the rand variable gets initialized. Nothing else happens.
+     */
     public Dice() {
         rand = new Random();
     }
 
+    /**
+     * Returns a random integer from 1 to the selected amount of faces.
+     * @return int
+     */
     public int rollDice() {
         return rand.nextInt(faceCount) + 1;
     }
 
+    /**
+     * Sets the faceCount value to the wanted amount of faces for the die.
+     * @param _faceCount sets the faceCount value.
+     */
     public static void setFaceCount(int _faceCount) {
         faceCount = _faceCount;
     }
