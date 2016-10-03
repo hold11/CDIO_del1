a7d2c813e26243e09c21557ed2149818e1a7e77a
Anders W. Olsen <awo@defalt.net>
2016-10-03 15:18:29 +0200

    DiceCup docs added

--- src/DiceCup.java
+++ src/DiceCup.java
@@ -9,11 +9,20 @@
  ||======================================||
  */
 
+/**
+ * This class uses the Dice class to roll multiple die. It handles the wanted amount
+ * of die in the project, and rolls them all.
+ * @version 1.0.0
+ */
 public class DiceCup {
     private Dice[] die;     // [] Because we have an Array of Dice objects
 
     private static int diceCount;
 
+    /**
+     * When the DiceCup gets constructed, an arrays gets initialized with the wanted amount of die. Therefore
+     * it is important that the diceCount variable gets set first (with the setDiceCount method).
+     */
     public DiceCup() {
         // We instantiate our Dice object array with the diceCount number of die
         die = new Dice[diceCount];
@@ -25,6 +34,10 @@ public class DiceCup {
         }
     }
 
+    /**
+     * Returns an array of all the rolled die. All values gets put in an array, and outputted.
+     * @return array of int's
+     */
     public int[] rollDie() {
         // We need an array of results, so every dice that gets rolled, gets analysed speerately in the GameLogic class
         int[] result = new int[diceCount];
@@ -38,10 +51,18 @@ public class DiceCup {
         return result;
     }
 
+    /**
+     * Sets the wanted amount of die in the project.
+     * @param _diceCount wanted amount of die.
+     */
     public static void setDiceCount(int _diceCount) {
         diceCount = _diceCount;
     }
 
+    /**
+     * Returns the specified amount of die chosen in the project.
+     * @return int
+     */
     public static int getDiceCount() {
         return diceCount;
     }
